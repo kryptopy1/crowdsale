2ede7fae44f94af6d9b3341acc2c6ad53be42eae
socialcode-rob1 <robbassani@gmail.com>
2017-09-25 12:05:45 -0400

    revised contracts to follow solium linting

--- contracts/KryptopyCrowdsale.sol
+++ contracts/KryptopyCrowdsale.sol
@@ -4,6 +4,7 @@ import "./KryptopyToken.sol";
 import "zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol";
 
+
 /**
  * @title KryptopyCrowdsale
  * @dev This is a fully fledged crowdsale.
@@ -17,122 +18,134 @@ import "zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol";
  */
 contract KryptopyCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
-  /*
-  *  Events
-  */
-  event PreICOStarted();
-  event PreICOSucceeded();
-  event PreICOFailed();
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+    /*
+    *  Events
+    */
+    event PreICOStarted();
+    event PreICOSucceeded();
+    event PreICOFailed();
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
-  /*
-  *  Constants
-  */
-  /*
-   * - Preparing: All contract initialization calls and variables have not been set yet
-   * - Prefunding: We have not passed start time yet
-   * - Funding: Active crowdsale
-   * - Success: Minimum funding goal reached
-   * - Failure: Minimum funding goal not reached before ending time
-   * - Finalized: The finalized has been called and succesfully executed
-   * - Refunding: Refunds are loaded on the contract for reclaim.
-   */
-  enum CrowdsaleProgress {
-    PREICO,
-    GOALSUCCESS,
-    GOALFAILED
-  }
-
-  /*
-  *  Storage
-  */
-  // the amount of WEI to remove at each step of the ICO from the Bonuses
-  uint256 public preIcoMin = 400000000000000000 wei;
-  // the amount of WEI to remove at each step of the ICO from the Bonuses
-  uint256 public preIcoMax = 10000000000000000000 wei;
-  // we do not want the contract to start before the start date we put Stopped as initial State to prevent that.
-  CrowdsaleProgress public crowdsaleProgress;
-  // amount of kpy sent
-  uint256 public kpySent;
-
-  /*
-  * Public functions
-  */
-  /** Constructor to initialize all variables, including Crowdsale variables
-  * @param _startBlock unix timestamp for start of ICO
-  * @param _endBlock unix timestamp for end of ICO
-  * @param _rate of ether to KrytopyToken in wei
-  * @param _goal minimum amount of funds to be raised in wei
-  * @param _cap max amount of funds to be raised in wei
-  * @param _wallet Address of the deployed Multisig wallet
-  */
-  function KryptopyCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet)
-    CappedCrowdsale(_cap)
-    FinalizableCrowdsale()
-    RefundableCrowdsale(_goal)
-    Crowdsale(_startBlock, _endBlock, _rate, _wallet)
-  {
-    //As goal needs to be met for a successful crowdsale
-    //the value needs to less or equal than a cap which is limit for accepted funds
-    require(_goal <= _cap);
-    startPreICO();
-  }
-
-  // overridden : Crowdsale.buyTokens(address beneficiary) payable
-  function buyTokens(address beneficiary)
-    public
-    payable
-  {
-    require(beneficiary != 0x0);
-    require(validPurchase());
-    require(msg.value >= preIcoMin);
-    require(msg.value <= preIcoMax);
-
-    uint256 weiAmount = msg.value;
-
-    uint256 exchangeBuffer = 1000000000;
-    uint256 tokensToSend = weiAmount.mul(rate).div(exchangeBuffer);
-
-    weiRaised = weiRaised.add(weiAmount);
-
-    // update state
-    token.mint(beneficiary, tokensToSend);
-    TokenPurchase(msg.sender, beneficiary, weiAmount, tokensToSend);
-
-    kpySent = kpySent.add(tokensToSend);
-    if(kpyGoalReached()) {
-      crowdsaleProgress = CrowdsaleProgress.GOALSUCCESS;
-      PreICOSucceeded();
+    /*
+    *  Constants
+    */
+    /*
+     * - Preparing: All contract initialization calls and variables have not been set yet
+     * - Prefunding: We have not passed start time yet
+     * - Funding: Active crowdsale
+     * - Success: Minimum funding goal reached
+     * - Failure: Minimum funding goal not reached before ending time
+     * - Finalized: The finalized has been called and succesfully executed
+     * - Refunding: Refunds are loaded on the contract for reclaim.
+     */
+    enum CrowdsaleProgress {
+      PREICO,
+      GOALSUCCESS,
+      GOALFAILED
     }
 
-    super.forwardFunds();
-  }
+    /*
+    *  Storage
+    */
+    // the amount of WEI to remove at each step of the ICO from the Bonuses
+    uint256 public preIcoMin = 400000000000000000 wei;
+    // the amount of WEI to remove at each step of the ICO from the Bonuses
+    uint256 public preIcoMax = 10000000000000000000 wei;
+    // we do not want the contract to start before the start date we put Stopped as initial State to prevent that.
+    CrowdsaleProgress public crowdsaleProgress;
+    // amount of kpy sent
+    uint256 public kpySent;
 
-  /*
-  * Internal functions
-  */
-  // overridden : Crowdsale.createTokenContract()
-  function createTokenContract()
-    internal
-    returns (MintableToken)
-  {
-    return new KryptopyToken();
-  }
+    /*
+    * Public functions
+    */
+    /** Constructor to initialize all variables, including Crowdsale variables
+    * @param _startBlock unix timestamp for start of ICO
+    * @param _endBlock unix timestamp for end of ICO
+    * @param _rate of ether to KrytopyToken in wei
+    * @param _goal minimum amount of funds to be raised in wei
+    * @param _cap max amount of funds to be raised in wei
+    * @param _wallet Address of the deployed Multisig wallet
+    */
+    function KryptopyCrowdsale(
+        uint256 _startBlock,
+        uint256 _endBlock,
+        uint256 _rate,
+        uint256 _goal,
+        uint256 _cap,
+        address _wallet
+      )
+      CappedCrowdsale(_cap)
+      FinalizableCrowdsale()
+      RefundableCrowdsale(_goal)
+      Crowdsale(_startBlock, _endBlock, _rate, _wallet)
+    {
+        //As goal needs to be met for a successful crowdsale
+        //the value needs to less or equal than a cap which is limit for accepted funds
+        require(_goal <= _cap);
+        startPreICO();
+    }
 
-  function startPreICO()
-    onlyOwner
-    internal
-  {
-    crowdsaleProgress = CrowdsaleProgress.PREICO;
-    PreICOStarted();
-  }
+    // overridden : Crowdsale.buyTokens(address beneficiary) payable
+    function buyTokens(address beneficiary)
+      public
+      payable
+    {
+        require(beneficiary != 0x0);
+        require(validPurchase());
+        require(msg.value >= preIcoMin);
+        require(msg.value <= preIcoMax);
 
-  function kpyGoalReached()
-    internal
-    constant
-    returns (bool)
-  {
-    uint256 kpyGoal = 2500000000000000000000;
-    return kpySent >= kpyGoal;
-  }
+        uint256 weiAmount = msg.value;
+
+        uint256 exchangeBuffer = 1000000000;
+        uint256 tokensToSend = weiAmount.mul(rate).div(exchangeBuffer);
+
+        weiRaised = weiRaised.add(weiAmount);
+
+        // update state
+        token.mint(beneficiary, tokensToSend);
+        TokenPurchase(
+            msg.sender,
+            beneficiary,
+            weiAmount,
+            tokensToSend
+        );
+
+        kpySent = kpySent.add(tokensToSend);
+        if (kpyGoalReached()) {
+            crowdsaleProgress = CrowdsaleProgress.GOALSUCCESS;
+            PreICOSucceeded();
+        }
+
+        super.forwardFunds();
+    }
+
+    /*
+    * Internal functions
+    */
+    // overridden : Crowdsale.createTokenContract()
+    function createTokenContract()
+      internal
+      returns (MintableToken)
+    {
+        return new KryptopyToken();
+    }
+
+    function startPreICO()
+      onlyOwner
+      internal
+    {
+        crowdsaleProgress = CrowdsaleProgress.PREICO;
+        PreICOStarted();
+    }
+
+    function kpyGoalReached()
+      internal
+      constant
+      returns (bool)
+    {
+        uint256 kpyGoal = 2500000000000000000000;
+        return kpySent >= kpyGoal;
+    }
 }
--- contracts/KryptopyToken.sol
+++ contracts/KryptopyToken.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.4.15;
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
 import "zeppelin-solidity/contracts/token/PausableToken.sol";
 
+
 /**
  * @title KryptopyToken
  *
@@ -29,87 +30,78 @@ import "zeppelin-solidity/contracts/token/PausableToken.sol";
  */
 contract KryptopyToken is MintableToken, PausableToken {
 
-  string public constant name = "Kryptopy Token";
-  string public constant symbol = "KPY";
-  string public constant version = "1.0";
-  uint8 public constant decimals = 10;
+    string public constant NAME = "Kryptopy Token";
+    string public constant SYMBOL = "KPY";
+    string public constant VERSION = "1.0";
+    uint8 public constant DECIMALS = 10;
 
-  //** Maximum total number of tokens ever created */
-  uint256 public constant tokenCap = 40000000 * (10 ** uint256(decimals));
-  //** Initial Supply */
-  uint256 public constant tokenReserve = 5000000 * (10 ** uint256(decimals));
+    //** Maximum total number of tokens ever created */
+    uint256 public constant TOKEN_CAP = 40000000 * (10 ** uint256(DECIMALS));
+    //** Initial Supply */
+    uint256 public constant TOKEN_RESERVE = 5000000 * (10 ** uint256(DECIMALS));
 
-  /**
-   * @dev Contructor that gives msg.sender all of existing tokens.
-   */
-  function KryptopyToken() MintableToken() {
-    owner = msg.sender;
-    totalSupply = tokenReserve;
-    balances[owner] = totalSupply;
-    Mint(owner, totalSupply);
-  }
-
-  /**
-   * @dev override MintableToken to check tokenCap and add a Transfer event from 0x0 to owner.
-   * @param _to The address that will receive the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool success) {
-    if (totalSupply.add(_amount) > tokenCap) {
-      return false;
+    /**
+     * @dev Contructor that gives msg.sender all of existing tokens.
+     */
+    function KryptopyToken() MintableToken() {
+        owner = msg.sender;
+        totalSupply = TOKEN_RESERVE;
+        balances[owner] = totalSupply;
+        Mint(owner, totalSupply);
     }
-    totalSupply = totalSupply.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    Mint(_to, _amount);
-    Transfer(0x0, _to, _amount);
-    return true;
-  }
 
-  /**
-  * @dev override BasicToken transfer to check if sender contains enough to transfer
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
-  function transfer(address _to, uint256 _value) returns (bool) {
-    if (balances[msg.sender] >= _value
-        && _value > 0
-        && balances[_to] + _value > balances[_to]
-        ) {
-      balances[msg.sender] = balances[msg.sender].sub(_value);
-      balances[_to] = balances[_to].add(_value);
-      Transfer(msg.sender, _to, _value);
-      return true;
+    /**
+     * @dev override MintableToken to check TOKEN_CAP and add a Transfer event from 0x0 to owner.
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool success) {
+        if (totalSupply.add(_amount) > TOKEN_CAP) {
+            return false;
+        }
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Mint(_to, _amount);
+        Transfer(0x0, _to, _amount);
+        return true;
     }
-    else {
-      return false;
+
+    /**
+    * @dev override BasicToken transfer to check if sender contains enough to transfer
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) returns (bool) {
+        if (balances[msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+            balances[msg.sender] = balances[msg.sender].sub(_value);
+            balances[_to] = balances[_to].add(_value);
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
 
-  /**
-   * @dev override StandardToken Transfer tokens from one address to another
-   * @param _from address The address which you want to send tokens from
-   * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amout of tokens to be transfered
-   */
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
-    uint _allowance = allowed[_from][msg.sender];
+    /**
+     * @dev override StandardToken Transfer tokens from one address to another
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amout of tokens to be transfered
+     */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+        uint _allowance = allowed[_from][msg.sender];
 
-    if (balances[_from] >= _value   // From a/c has balance
-        && _allowance >= _value    // Transfer approved
-        && _value > 0              // Non-zero transfer
-        && balances[_to] + _value > balances[_to]  // Overflow check
-        ){
+        if (balances[_from] >= _value && _allowance >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
 
-      allowed[_from][msg.sender] = _allowance.sub(_value);
-      balances[_from] = balances[_from].sub(_value);
-      balances[_to] = balances[_to].add(_value);
+            allowed[_from][msg.sender] = _allowance.sub(_value);
+            balances[_from] = balances[_from].sub(_value);
+            balances[_to] = balances[_to].add(_value);
 
-      Transfer(_from, _to, _value);
-      return true;
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
     }
-    else {
-      return false;
-    }
-  }
 }
