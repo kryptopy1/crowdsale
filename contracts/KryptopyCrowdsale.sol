1aae69d892f5f4c38e91affda015403a476e15e0
socialcode-rob1 <robbassani@gmail.com>
2017-09-25 00:00:35 -0400

    crowdsale is preICO focused

--- contracts/KryptopyCrowdsale.sol
+++ contracts/KryptopyCrowdsale.sol
@@ -23,14 +23,6 @@ contract KryptopyCrowdsale is CappedCrowdsale, RefundableCrowdsale {
   event PreICOStarted();
   event PreICOSucceeded();
   event PreICOFailed();
-  event ICOStarted();
-  event ICOStep1();
-  event ICOStep2();
-  event ICOStep3();
-  event ICOStep4();
-  event ICOStep5();
-  event ICOStep6();
-  event ICOFinished();
   event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
   /*
@@ -46,39 +38,22 @@ contract KryptopyCrowdsale is CappedCrowdsale, RefundableCrowdsale {
    * - Refunding: Refunds are loaded on the contract for reclaim.
    */
   enum CrowdsaleProgress {
-    STARTED,
-    STOPPED,
     PREICO,
-    ICO0,
-    ICO1,
-    ICO2,
-    ICO3,
-    ICO4,
-    ICO5,
     GOALSUCCESS,
-    GOALFAILED,
-    ICOENDED
+    GOALFAILED
   }
 
   /*
   *  Storage
   */
-  // in pre-ICO 200 KPY per ether invested we double the investment per ether in pre-ICO
-  uint256 public initPreICOBonus = 200000000000000000 wei;
-  // in ICO 125 KPY per ether invested we double the investment per ether in ICO
-  uint256 public initICOBonus = 125000000000000000 wei;
   // the amount of WEI to remove at each step of the ICO from the Bonuses
-  uint256 public bonusStepMin =  25000000000000000 wei;
+  uint256 public preIcoMin =  400000000000000000 wei;
+  // the amount of WEI to remove at each step of the ICO from the Bonuses
+  uint256 public preIcoMax =  10000000000000000000 wei;
   // we do not want the contract to start before the start date we put Stopped as initial State to prevent that.
   CrowdsaleProgress public crowdsaleProgress;
-
-  /*
-   *  Modifiers
-   */
-   modifier isState(CrowdsaleProgress _progress) {
-       require(_progress == crowdsaleProgress);
-       _;
-   }
+  // amount of kpy sent
+  uint256 public kpySent = 0;
 
   /*
   * Public functions
@@ -98,17 +73,6 @@ contract KryptopyCrowdsale is CappedCrowdsale, RefundableCrowdsale {
     RefundableCrowdsale(_goal)
     Crowdsale(_startBlock, _endBlock, _rate, _wallet)
   {
-    /*require(_startBlock >= block.number);
-    require(_endBlock >= _startBlock);
-    require(_rate > 0);
-    require(_wallet != 0x0);
-
-    token = createTokenContract(_token);
-    startBlock = _startBlock;
-    endBlock = _endBlock;
-    rate = _rate;
-    wallet = _wallet;*/
-
     //As goal needs to be met for a successful crowdsale
     //the value needs to less or equal than a cap which is limit for accepted funds
     require(_goal <= _cap);
@@ -122,20 +86,27 @@ contract KryptopyCrowdsale is CappedCrowdsale, RefundableCrowdsale {
   {
     require(beneficiary != 0x0);
     require(validPurchase());
+    require(msg.value >= preIcoMin);
+    require(msg.value <= preIcoMax);
 
     uint256 weiAmount = msg.value;
 
-    // get bonus based on step
-    // calculate token amount to be created
-    uint256 weiAmountPlusBonus = bonus(weiAmount);
-    uint256 tokensToSend = weiAmountPlusBonus.mul(rate);
+    // 2 nanokpy(x10m) / wei (normal)
+    // 265 nanokpy(x1b) / wei (incl. bonus)
+    uint256 exchangeBuffer = 1000000000;
+    uint256 tokensToSend = weiAmount.mul(rate).div(exchangeBuffer);
 
     weiRaised = weiRaised.add(weiAmount);
-    setSteps();
 
     // update state
     token.mint(beneficiary, tokensToSend);
     TokenPurchase(msg.sender, beneficiary, weiAmount, tokensToSend);
+
+    kpySent = kpySent.add(tokensToSend);
+    if(kpyGoalReached()) {
+      crowdsaleProgress = CrowdsaleProgress.GOALSUCCESS;
+      PreICOSucceeded();
+    }
 
     super.forwardFunds();
   }
@@ -159,96 +130,7 @@ contract KryptopyCrowdsale is CappedCrowdsale, RefundableCrowdsale {
     PreICOStarted();
   }
 
-  function startICO()
-    onlyOwner
-    isState(CrowdsaleProgress.GOALSUCCESS)
-    internal
-  {
-    crowdsaleProgress = CrowdsaleProgress.ICO0;
-    ICOStarted();
-    ICOStep1();
+  function kpyGoalReached() public constant returns (bool) {
+    return kpySent >= 2500000000000000;
   }
-
-  // we double check to make sure the sale is not Stopped or at last step. Last step of ICO is not having any bonuses
-  // function to calculate bonus based on step of the ICO or pre-ICO
-  function bonus(uint256 weiAmount)
-    internal
-    returns (uint256 bonus)
-  {
-    if(crowdsaleProgress == CrowdsaleProgress.PREICO) {
-      bonus = initPreICOBonus.add(weiAmount);
-    }
-    if(crowdsaleProgress == CrowdsaleProgress.ICO0) {
-      bonus = initICOBonus.add(weiAmount);
-    }
-    if(crowdsaleProgress == CrowdsaleProgress.ICO1) {
-      bonus = initICOBonus.sub(bonusStepMin);
-      bonus = initICOBonus.add(weiAmount);
-    }
-    if(crowdsaleProgress == CrowdsaleProgress.ICO2) {
-      bonus = initICOBonus.sub(bonusStepMin.mul(2));
-      bonus = initICOBonus.add(weiAmount);
-    }
-    if(crowdsaleProgress == CrowdsaleProgress.ICO3) {
-      bonus = initICOBonus.sub(bonusStepMin.mul(3));
-      bonus = initICOBonus.add(weiAmount);
-    }
-    if(crowdsaleProgress == CrowdsaleProgress.ICO4) {
-      bonus = initICOBonus.sub(bonusStepMin.mul(4));
-      bonus = initICOBonus.add(weiAmount);
-    }
-    if(crowdsaleProgress == CrowdsaleProgress.ICO5) {
-        bonus = weiAmount;
-    }
-    return bonus;
-  }
-
-
-  //ICO Steps are calculated based on the amount of funds raised.
-  function setSteps()
-    internal
-  {
-    if(weiRaised >= 15384620000000000000000 && weiRaised < 16666670000000000000000) {
-      crowdsaleProgress = CrowdsaleProgress.ICO1;
-      ICOStep2();
-    } else if(weiRaised >= 16666670000000000000000 && weiRaised < 18181820000000000000000) {
-      crowdsaleProgress = CrowdsaleProgress.ICO2;
-      ICOStep3();
-    } else if(weiRaised >= 18181820000000000000000 && weiRaised < 20000000000000000000000) {
-      crowdsaleProgress = CrowdsaleProgress.ICO3;
-      ICOStep4();
-    } else if(weiRaised >= 20000000000000000000000 && weiRaised < 22222220000000000000000) {
-      crowdsaleProgress = CrowdsaleProgress.ICO4;
-      ICOStep5();
-    } else if(weiRaised >= 22222220000000000000000 && weiRaised < 25000000000000000000000) {
-      crowdsaleProgress = CrowdsaleProgress.ICO5;
-      ICOStep6();
-    } else if(weiRaised >= 25000000000000000000000) {
-      endICO();
-    }
-  }
-
-  // will do the deposit action from RefundVault
-  // overridden : RefundableCrowdsale.forwardFunds() internal
-  function forwardFunds()
-    internal
-  {
-    // we calculate the bonus to add to the value in wei
-    uint256 totalValue = bonus(msg.value);
-    vault.deposit.value(totalValue)(msg.sender);
-  }
-
-  // we ended the ICO and everything went smoothly yay !
-  function endICO()
-    internal
-  {
-    // end routines here
-    require(super.hasEnded() == true);
-    crowdsaleProgress = CrowdsaleProgress.ICOENDED;
-  }
-
-  /*
-  * Web3 call functions
-  */
-
 }
--- test/KryptopyCrowdsale.js
+++ test/KryptopyCrowdsale.js
@@ -16,10 +16,22 @@ const KryptopyToken = artifacts.require('../contracts/KryptopyToken.sol');
 
 contract('KryptopyCrowdsale', function ([_, investor, wallet, purchaser]) {
 
-  const rate = new BigNumber(1000);
-  const cap = new web3.BigNumber(12500000000000000000000);
-  const goal = new web3.BigNumber(2500000000000000000000);
+  const rate = new BigNumber(265);
+  const cap = new web3.BigNumber(12500000000000000000000); // wei (12500 eth)
+  const goal = new web3.BigNumber(2500000000000000000000); // wei (2500 eth)
+  const lessThanGoal = ether(500);
   const lessThanCap = ether(1);
+  const valueLessThanPreIcoMin = new web3.BigNumber(300000000000000000); // wei (0.3 eth)
+  const valueMoreThanPreIcoMax = ether(10)
+  const value = ether(1)
+
+  const expectedTokenAmount = rate.mul(value)
+
+  var CrowdsaleProgress = [
+    "PREICO",
+    "GOALSUCCESS",
+    "GOALFAILED"
+  ];
 
   before(async function() {
     //Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
@@ -36,17 +48,98 @@ contract('KryptopyCrowdsale', function ([_, investor, wallet, purchaser]) {
     this.token = KryptopyToken.at(await this.crowdsale.token());
   });
 
-  it('should return the correct state and bonus values after construction', async function () {
-    let preICOBonus = await this.crowdsale.initPreICOBonus();
+  describe('upon construction', function () {
 
-    let icoBonus = await this.crowdsale.initICOBonus();
+    it('should return the correct state and bonus values after construction', async function () {
+      let preICOBonus = new web3.BigNumber(200000000000000000)
+      // let icoBonus = new web3.BigNumber(125000000000000000)
+      // let bonusStepMin = new web3.BigNumber(25000000000000000)
+      let progressIndex = new web3.BigNumber(CrowdsaleProgress.indexOf("PREICO"))
 
-    let bonusStepMin = await this.crowdsale.bonusStepMin();
+      let initPreICOBonus = await this.crowdsale.initPreICOBonus()
+      initPreICOBonus.should.be.bignumber.equal(preICOBonus)
 
-    let progess = await this.crowdsale.crowdsaleProgress();
+      // let initICOBonus = await this.crowdsale.initICOBonus()
+      // initICOBonus.should.be.bignumber.equal(icoBonus)
+      //
+      // let initBonusStepMin = await this.crowdsale.bonusStepMin()
+      // initBonusStepMin.should.be.bignumber.equal(bonusStepMin)
 
-    // await this.crowdsale.send(lessThanCap).should.be.fulfilled;
-  });
+      let crowdsaleProgress = await this.crowdsale.crowdsaleProgress();
+      crowdsaleProgress.should.be.bignumber.equal(progressIndex)
+    })
+  })
+
+  describe('during PRE-ICO event', function () {
+
+    it('should accept token purchases during pre-sale', async function () {
+      let progressIndex = new web3.BigNumber(CrowdsaleProgress.indexOf("PREICO"))
+
+      await increaseTimeTo(this.startTime)
+      await this.crowdsale.send(value).should.be.fulfilled
+      await this.crowdsale.buyTokens(investor, {value: value, from: purchaser}).should.be.fulfilled
+
+      let crowdsaleProgress = await this.crowdsale.crowdsaleProgress();
+      crowdsaleProgress.should.be.bignumber.equal(progressIndex)
+    })
+
+    it('should accept token purchases during pre-sale after goal has been met', async function () {
+      let progressIndex = new web3.BigNumber(CrowdsaleProgress.indexOf("GOALSUCCESS"))
+
+      await increaseTimeTo(this.startTime)
+      await this.crowdsale.send(goal).should.be.fulfilled
+      await this.crowdsale.buyTokens(investor, {value: goal, from: purchaser}).should.be.fulfilled
+
+      let crowdsaleProgress = await this.crowdsale.crowdsaleProgress();
+      crowdsaleProgress.should.be.bignumber.equal(progressIndex)
+
+      await this.crowdsale.send(value).should.be.fulfilled
+      await this.crowdsale.buyTokens(investor, {value: value, from: purchaser}).should.be.fulfilled
+
+    })
+
+    it('should reject token purchases after pre-ico supply has been exhausted', async function () {
+      let progressIndex = new web3.BigNumber(CrowdsaleProgress.indexOf("GOALSUCCESS"))
+
+      await increaseTimeTo(this.startTime)
+      await this.crowdsale.send(cap).should.be.fulfilled
+      await this.crowdsale.buyTokens(investor, {value: cap, from: purchaser}).should.be.fulfilled
+
+      let crowdsaleProgress = await this.crowdsale.crowdsaleProgress();
+      crowdsaleProgress.should.be.bignumber.equal(progressIndex)
+
+      await this.crowdsale.send(value).should.be.rejectedWith(EVMThrow)
+      await this.crowdsale.buyTokens(investor, {value: value, from: purchaser}).should.be.rejectedWith(EVMThrow)
+    })
+
+    // it('should log purchase', async function () {
+    //   const {logs} = this.crowdsale.buyTokens(investor, {value: value, from: purchaser})
+    //
+    //   const event = logs.find(e => e.event === 'TokenPurchase')
+    //   console.log(event)
+    //   should.exist(event)
+    //   event.args.purchaser.should.equal(investor)
+    //   event.args.beneficiary.should.equal(investor)
+    //   event.args.value.should.be.bignumber.equal(value)
+    //   event.args.amount.should.be.bignumber.equal(expectedTokenAmount)
+    // })
+
+    // it('should log purchase', async function () {
+    //   const {logs} = await this.crowdsale.sendTransaction({value: value, from: investor})
+    //
+    //   const event = logs.find(e => e.event === 'TokenPurchase')
+    //   console.log(event)
+    //   should.exist(event)
+    //   event.args.purchaser.should.equal(investor)
+    //   event.args.beneficiary.should.equal(investor)
+    //   event.args.value.should.be.bignumber.equal(value)
+    //   event.args.amount.should.be.bignumber.equal(expectedTokenAmount)
+    // })
+  })
+
+  describe('druing ICO event', function() {
+
+  })
 
   // send money
 
@@ -55,13 +148,5 @@ contract('KryptopyCrowdsale', function ([_, investor, wallet, purchaser]) {
   // should finalize
 
   // should refund
-
-
-
-  // it('should start PreICO', async function () {
-  //   await this.crowdsale.startPreICO();
-  //   const progress = await this.crowdsale.crowdsaleProgress();
-  //   progress.should.equal('2');
-  // });
 
 });
